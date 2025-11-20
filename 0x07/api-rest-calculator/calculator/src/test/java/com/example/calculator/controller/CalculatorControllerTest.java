a713a2c3cb03f74bb2b8ae901d0d4d2140f13e68
Yuri Fernandes <yuri_fernandes2000@hotmail.com>
2025-11-20 16:58:48 -0300

    0. Criando um aplicação API REST Calculator + JUNIT

--- 0x07/api-rest-calculator/calculator/src/test/java/com/example/calculator/controller/CalculatorControllerTest.java
+++ 0x07/api-rest-calculator/calculator/src/test/java/com/example/calculator/controller/CalculatorControllerTest.java
@@ -5,9 +5,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
--- 0x07/api-rest-calculator/src/test/java/com/example/calculator/model/CalculatorTest.java
+++ 0x07/api-rest-calculator/src/test/java/com/example/calculator/model/CalculatorTest.java
@@ -1,24 +1,17 @@
 package com.example.calculator.model;
 
-import static org.mockito.ArgumentMatchers.anyDouble;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.mock;
-
 import org.junit.jupiter.api.Test;
 public class CalculatorTest {
-    private final Calculator calculator = mock(Calculator.class);
+    private final Calculator calculator = new Calculator();
 
     @Test
     void sumTest() {
-        when(calculator.sum(anyDouble(), anyDouble())).thenReturn(10.0);
         Double result = calculator.sum(4.0, 6.0);
         assert result.equals(10.0);
     }
 
     @Test
     public void numbersNullSumTest() {
-        when(calculator.sum(null, null)).thenThrow(new NullPointerException("Número 1 e número 2 são obrigatórios."));
         try {
             calculator.sum(null, null);
         } catch (NullPointerException e) {
@@ -28,21 +21,18 @@ public class CalculatorTest {
 
     @Test
     void subTest() {
-        when(calculator.sub(anyDouble(), anyDouble())).thenReturn(5.0);
         Double result = calculator.sub(10.0, 5.0);
         assert result.equals(5.0);
     }
 
     @Test
     void divideTest() {
-       when(calculator.divide(anyDouble(), anyDouble())).thenReturn(2.0);
        Double result = calculator.divide(10.0, 5.0);
        assert result.equals(2.0);
     }
 
     @Test
     public void divisionByZeroTest() {
-        when(calculator.divide(anyDouble(), eq(0.0))).thenThrow(new ArithmeticException("Divisão por zero não é permitida."));
         try {
             calculator.divide(10.0, 0.0);
         } catch (ArithmeticException e) {
@@ -52,28 +42,24 @@ public class CalculatorTest {
 
     @Test
     void factorialTest() {
-        when(calculator.factorial(5)).thenReturn(120);
         Integer result = calculator.factorial(5);
         assert result.equals(120);
     }
 
     @Test
     void integerToBinaryTest() {
-       when(calculator.integerToBinary(10)).thenReturn(1010);
        Integer result = calculator.integerToBinary(10);
        assert result.equals(1010);
     }
 
     @Test
     void integerToHexadecimalTest() {
-       when(calculator.integerToHexadecimal(255)).thenReturn("FF");
        String result = calculator.integerToHexadecimal(255);
        assert result.equals("FF");
     }
 
     @Test
     void calculeDayBetweenDateTest() {
-        when(calculator.calculeDayBetweenDate(java.time.LocalDate.of(2023,1,1), java.time.LocalDate.of(2023,1,31))).thenReturn(30);
         Integer result = calculator.calculeDayBetweenDate(java.time.LocalDate.of(2023,1,1), java.time.LocalDate.of(2023,1,31));
         assert result.equals(30);
     }
